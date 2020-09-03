ed26707697b166916ac063339e5c37176ad7a53a
wangwenzhu <bcsxmail@gmail.com>
2020-09-04 01:03:44 +0800

    添加echo功能

--- tcp_server/src/main.rs
+++ tcp_server/src/main.rs
@@ -1,17 +1,15 @@
 use std::io::prelude::*;
-use std::io::BufReader;
-use std::io::BufWriter;
+use std::io::{BufReader, BufWriter, Write};
+
 use std::net::TcpListener;
 use std::thread;
 
 
-fn main() {
+fn main() -> std::io::Result<()> {
     //通过通过TcpListener方法监听本地8080端口，通过unwrap方法来处理异常
     let l = TcpListener::bind("127.0.0.1:8080").unwrap(); 
     //通过迭代程序处理incoming的返回值
     for stream in l.incoming() {
-        //创建一个线程来处理TCP接收到的请求
-        thread::spawn(move || {
             //创建一个变量stream来存储接收到的对象
             let stream = stream.unwrap(); 
             //添加一个缓存存储stream的数据，存储到reader变量
@@ -21,19 +19,23 @@ fn main() {
             //通过迭代程序逐行处理reader中的文本
             for line in reader.lines() {
                 //通过line变量覆盖原line变量
-                let line = line.unwrap();
+                let mut line = line.unwrap();
+                line.push('\n');
+                // let line = line.into_bytes();
                 //输出line的值
-                println!("收到TCP请求，内容为：{}", line);
+                println!("收到TCP请求，内容为：{:?}", &line);
                 //将回应TCP请求的字节串写入缓存
-                writer.write_all(b"got it.\n").unwrap();
+                writer.write_all(line.as_bytes()).unwrap();
                 //发出对TCP请求的回应，并处理异常
                 match writer.flush() {
                     //回应成功后在屏幕打印出成功信息
-                    Ok(n) => println!("已成功回应{:?}",n),
+                    Ok(_n) => {
+                        println!("已成功回应{}",&line);
+                    },
                     //异常后在屏幕打印出异常信息
                     Err(err) => println!("{:?}", err),
                 };
             }
-        });
     }
+    Ok(())
 }
